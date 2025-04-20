e72030ea487212ffdff86a29000406cf4ef9c710
ByteWave1014 <jvillaltamatos3007@gmail.com>
2025-04-20 18:50:17 -0500

    sm8550-common: Adding missing audio effect libs
    - With this the dolby will work
    
    Change-Id: I8a24adfcbe3ea2c76a57d91cb8da50b9e10dd16c
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- sm8550-common-vendor.mk
+++ sm8550-common-vendor.mk
@@ -535,6 +535,7 @@ PRODUCT_PACKAGES += \
     libdisplayqos \
     libdisplayskuutils \
     libdlbdsservice \
+    libdlbpreg \
     libdolbyottcameracontrol \
     libdpmqmihal \
     libdpps \
@@ -770,6 +771,7 @@ PRODUCT_PACKAGES += \
     libssd \
     libstandbyfeature \
     libstfactory-vendor \
+    libswspatializer_ext \
     libsynergy_loc_api \
     libsys_info_cache \
     libsystem_health_mon \
@@ -839,10 +841,14 @@ PRODUCT_PACKAGES += \
     se_nq_extn_client \
     sensors.ssc \
     libasphere \
+    libdlbvol \
+    libhwdap \
     libmisoundfx \
     libquasar \
     libshoebox \
+    libspatializer \
     libswgamedap \
+    libswspatializer \
     libswvqe \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
